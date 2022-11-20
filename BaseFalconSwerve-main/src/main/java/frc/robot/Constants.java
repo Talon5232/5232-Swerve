f4c929a61f95e55b1517dc5750dc2f98dace3041
Talon5232 <61335902+Talon5232@users.noreply.github.com>
2022-11-19 22:21:20 -0600

    t
    t

--- BaseFalconSwerve-main/src/main/java/frc/robot/Constants.java
+++ BaseFalconSwerve-main/src/main/java/frc/robot/Constants.java
@@ -16,16 +16,18 @@ public final class Constants {
         public static final boolean invertGyro = false; // Always ensure Gyro is CCW+ CW-
 
         /* Drivetrain Constants */
-        public static final double trackWidth = Units.inchesToMeters(21.73);
-        public static final double wheelBase = Units.inchesToMeters(21.73);
+        //trackwidth=x direction
+        public static final double trackWidth = Units.inchesToMeters(24);
+        //wheelbase=z direction
+        public static final double wheelBase = Units.inchesToMeters(24);
         public static final double wheelDiameter = Units.inchesToMeters(3.94);
         public static final double wheelCircumference = wheelDiameter * Math.PI;
 
         public static final double openLoopRamp = 0.25;
         public static final double closedLoopRamp = 0.0;
 
-        public static final double driveGearRatio = (6.86 / 1.0); //6.86:1
-        public static final double angleGearRatio = (12.8 / 1.0); //12.8:1
+        public static final double driveGearRatio = (6.75 / 1.0); //6.86:1
+        public static final double angleGearRatio = (21.42 / 1.0); //12.8:1
 
         public static final SwerveDriveKinematics swerveKinematics = new SwerveDriveKinematics(
                 new Translation2d(wheelBase / 2.0, trackWidth / 2.0),
@@ -35,19 +37,19 @@ public final class Constants {
 
         /* Swerve Current Limiting */
         public static final int angleContinuousCurrentLimit = 25;
-        public static final int anglePeakCurrentLimit = 40;
+        public static final int anglePeakCurrentLimit = 35;
         public static final double anglePeakCurrentDuration = 0.1;
         public static final boolean angleEnableCurrentLimit = true;
 
         public static final int driveContinuousCurrentLimit = 35;
-        public static final int drivePeakCurrentLimit = 60;
+        public static final int drivePeakCurrentLimit = 50;
         public static final double drivePeakCurrentDuration = 0.1;
         public static final boolean driveEnableCurrentLimit = true;
 
         /* Angle Motor PID Values */
-        public static final double angleKP = 0.6;
+        public static final double angleKP = 0.01;//FIXME
         public static final double angleKI = 0.0;
-        public static final double angleKD = 12.0;
+        public static final double angleKD = 12.0;//FIXME
         public static final double angleKF = 0.0;
 
         /* Drive Motor PID Values */
@@ -61,17 +63,16 @@ public final class Constants {
         public static final double driveKV = (2.44 / 12);
         public static final double driveKA = (0.27 / 12);
 
-        /* Swerve Profiling Values */
-        public static final double maxSpeed = 4.5; //meters per second
-        public static final double maxAngularVelocity = 11.5;
-
+     /* Swerve Profiling Values */
+        public static final double maxSpeed = 4.96; //meters per second 
+        public static final double maxAngularVelocity = 10.0666;
         /* Neutral Modes */
         public static final NeutralMode angleNeutralMode = NeutralMode.Coast;
         public static final NeutralMode driveNeutralMode = NeutralMode.Brake;
 
         /* Motor Inverts */
-        public static final boolean driveMotorInvert = false;
-        public static final boolean angleMotorInvert = false;
+        public static final boolean driveMotorInvert = true;
+        public static final boolean angleMotorInvert = true;
 
         /* Angle Encoder Invert */
         public static final boolean canCoderInvert = false;
@@ -86,7 +87,7 @@ public final class Constants {
             public static final SwerveModuleConstants constants = 
                 new SwerveModuleConstants(driveMotorID, angleMotorID, canCoderID, angleOffset);
         }
-
+        //FIXCANIDS
         /* Front Right Module - Module 1 */
         public static final class Mod1 {
             public static final int driveMotorID = 3;
